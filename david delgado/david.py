bd3efcbf01ea7ced7cf77090b84a32cf3983f77d
orlando <orlansonpapu69@gmail.com>
2026-08-25 10:14:35 -0400

    reto 17

---
+++ david delgado/david.py
@@ -0,0 +1,8 @@
+#determinar edad minima para votar 
+
+edad = int(input("Introduce tu edad: "))
+
+if edad >= 18:
+    print("Cumples con la edad mínima de 18 años.")
+else:
+    print("Eres menor de 18 años.")
